78dee22315f4afdda3a9b7799954ad1c1c50271b
ViciousDecay <lhrielo@gmail.com>
2017-04-30 14:52:10 +0200

    Arreglado menú navegación de login y registro

--- javascript/compruebalog.js
+++ javascript/compruebalog.js
@@ -8,6 +8,7 @@ if(window.localStorage){
 	else{
 		nav='<ul><li><label for="ckb-menu"><span>&equiv;</span></label></li><li><a href="index.html"><span class="icon-home"></span>Indice</a></li><li><a href="buscar.html"><span class="icon-search"></span>Buscar</a></li><li><a href="login.html"><span class="icon-login"></span>Login</a></li><li><a href="registro.html"><span class="icon-user-plus"></span>Registro</a></li><li><a href=""><span class="icon-logout"></span>Logout</a></li><li><a href="nueva-entrada.html"><span class="icon-doc-new"></span>Nueva entrada</a></li></ul>'
 	}
+	document.getElementById("navegacion").innerHTML=nav;
 }
 
 function logout(){
